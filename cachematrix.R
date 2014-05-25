df1d67449e1df229c8122256f8c187c23df63c39
rmsharp <rmsharp@me.com>
2014-05-25 13:12:41 -0500

    Added better function descriptions
    Used function descriptions from assignment as templates

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,12 @@
 ## Write a short comment describing this function
 #' Returns function list for use by cacheSolve to return the inverse of a matrix.
 #' If the inverse is cached, it is returned.
+#' The list contains functions to
+#' set the value of the matrix
+#' get the value of the matrix
+#' set the value of the inverted matrix
+#' get the value of the inverted matrix
+
 makeCacheMatrix <- function(x = matrix()) {
     i <- NULL
     set <- function(y) {
@@ -20,7 +26,12 @@ makeCacheMatrix <- function(x = matrix()) {
   
 
 ## Write a short comment describing this function
-
+#' Returns the inverse of the matrix in the list object created by 
+#' makeCacheMatrix.
+#' It first checks to see if the inverse of the matrix has already been 
+#' calculated. If so, it gets the inverse from the cache and skips the 
+#' computation. Otherwise, it calculates the inverse of the data and sets 
+#' the value of the inverse in the cache via the setinverse function.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   i <- x$getinverse()
